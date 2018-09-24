4167862602f846b87812ecae6ce87d2fa137ebf2
jdm79 <james.malvern@gmail.com>
2018-09-24 21:48:10 +0100

    add test for score method, add code to pass

--- lib/Game.rb
+++ lib/Game.rb
@@ -1,5 +1,14 @@
 class Game
+
+  def initialize 
+    @score = []
+  end
   def roll(num)
+    @score.push(num)
     num
+  end
+
+  def score 
+    @score.sum
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,4 +7,16 @@ describe "bowling" do
       expect(game.roll(3)).to eq 3
     end
   end
+
+  describe "rolls a ball" do
+    game = Game.new
+    it "should returns the score for the game" do
+      game = Game.new
+      game.roll(4)
+      game.roll(5)
+      game.roll(6)
+      game.roll(7)
+      expect(game.score()).to eq 22
+    end
+  end
 end
